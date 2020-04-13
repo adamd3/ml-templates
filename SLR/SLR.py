ab1dfa3a13b75cf7bb81c41b39e0aebd46a873bf
adam <ad866@cam.ac.uk>
2020-04-13 14:18:59 +0200

    Rename directories

--- SLR/SLR.py
+++ SLR/SLR.py
@@ -11,9 +11,6 @@ dataset = pd.read_csv('Salary_Data.csv')
 X = dataset.iloc[:, :-1].values #independent variable (years experience)
 y = dataset.iloc[:, 2].values #dependent variable (salary)
 
-##------------------------------------------------------------------------------
-## Pre-processing
-##------------------------------------------------------------------------------
 # Splitting the dataset into the Training set and Test set
 # (for simplicity, put 10 in the Test set and 20 in the Training set)
 X_train, X_test, y_train, y_test = train_test_split(
@@ -28,9 +25,7 @@ X_test = sc_X.transform(X_test)
 sc_y = StandardScaler()
 y_train = sc_y.fit_transform(y_train)"""
 
-##------------------------------------------------------------------------------
-## Fit SLR to the Training set
-##------------------------------------------------------------------------------
+# Fit Simple Linear Regression to the Training set
 regressor = LinearRegression() # object of the LinearRegression class
 regressor.fit(X_train, y_train) #fit method to fit object to the training set
 
@@ -38,12 +33,10 @@ regressor.fit(X_train, y_train) #fit method to fit object to the training set
 y_pred_train = regressor.predict(X_train)
 
 # Predicting the results using the Test set
-# NOTE: we don't plot these - just manually inspect them
+# NOTE: we don't plot these - it's just so manually inspect them
 y_pred_test = regressor.predict(X_test)
 
-##------------------------------------------------------------------------------
-## Plot the Training set data vs regression line
-##------------------------------------------------------------------------------
+# plot the Training set data vs our regression line
 plt.scatter(X_train, y_train, color = 'red') #data points
 plt.plot(X_train, y_pred_train, color = 'blue') #regression line
 plt.title('Salary vs Experience (Training set)')
@@ -51,10 +44,8 @@ plt.xlabel('Years of Experience')
 plt.ylabel('Salary')
 plt.show()
 
-##------------------------------------------------------------------------------
-## Plot the Test set data vs regression line
-## (NB: regression line is still based on the Training dataset)
-##------------------------------------------------------------------------------
+# plot the Test set data vs our regression line
+# NB: the regression line is still based on the TRAINING dataset
 plt.scatter(X_test, y_test, color = 'red') #data points
 plt.plot(X_train, y_pred_train, color = 'blue') #regression line
 plt.title('Salary vs Experience (Test set)')
