b84d305a3b4697b990aec1dc0776b3a4ee9fc9f6
adam <ad866@cam.ac.uk>
2020-04-17 13:33:33 +0200

    Updating PLR method

---
+++ PLR/PLR.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+##------------------------------------------------------------------------------
+## Import libraries
+##------------------------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.model_selection import train_test_split
+import statsmodels.api as sm
+
+
+##------------------------------------------------------------------------------
+## Data pre-processing
+##------------------------------------------------------------------------------
+dataset = pd.read_csv('Position_Salaries.csv')
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
+
+# Splitting the dataset into the Training set and Test set
+"""
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size = 0.2, random_state = 0
+)
+"""
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)"""
+
+##------------------------------------------------------------------------------
+## Fitting Polynomial Regression
+##------------------------------------------------------------------------------
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+poly_reg.fit(X_poly, y)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+##------------------------------------------------------------------------------
+## Visualising the results
+##------------------------------------------------------------------------------
+plt.scatter(X, y, color = 'red')
+plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+# For higher resolution and smoother curve
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y, color = 'red')
+plt.plot(
+    X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue'
+)
+plt.title('Truth or Bluff (Polynomial Regression)')
+plt.xlabel('Position level')
+plt.ylabel('Salary')
+plt.show()
+
+##------------------------------------------------------------------------------
+## Predicting a new result
+##------------------------------------------------------------------------------
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
