b68310cc75ee8acc3b352eec664cd3ed1c3eaa8d
adam <ad866@cam.ac.uk>
2020-04-25 12:56:43 +0200

    modify call to contourf

--- Classification/Random_forest_classification/RFC.py
+++ Classification/Random_forest_classification/RFC.py
@@ -61,8 +61,11 @@ X1, X2 = np.meshgrid(
         start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01
         )
     )
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.contourf(
+    X1, X2,
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+    alpha = 0.75, cmap = ListedColormap(('red', 'green'))
+    )
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -84,8 +87,11 @@ X1, X2 = np.meshgrid(
         start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01
         )
     )
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.contourf(
+    X1, X2,
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+    alpha = 0.75, cmap = ListedColormap(('red', 'green'))
+)
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
